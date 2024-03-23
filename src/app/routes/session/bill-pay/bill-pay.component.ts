a28d8d06dd9c1c5950a59c3e19f8f6c5984f9c19
David Pineda <david.pineda@free4m.io>
2024-03-23 16:07:43 -0400

    ng generate component routes/session + sub routes

---
+++ src/app/routes/session/bill-pay/bill-pay.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bill-pay',
+  standalone: true,
+  imports: [],
+  templateUrl: './bill-pay.component.html',
+  styleUrl: './bill-pay.component.scss'
+})
+export class BillPayComponent {
+
+}
