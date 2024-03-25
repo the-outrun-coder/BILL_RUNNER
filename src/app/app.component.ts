290535c9a3cf49185e4578060b7c5abc4d0c63ad
David Pineda <david.pineda@free4m.io>
2024-03-25 17:43:07 -0400

    start the LayoutMainComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,13 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { LayoutMainComponent } from './layout/layout-main/layout-main.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
 	imports: [
+		LayoutMainComponent,
 		RouterOutlet,
 		MatIconModule,
 		MatMenuModule,
---
+++ src/app/layout/layout-main/layout-main.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'brun-layout-main',
+  standalone: true,
+  imports: [],
+  templateUrl: './layout-main.component.html',
+  styleUrl: './layout-main.component.scss'
+})
+export class LayoutMainComponent {
+
+}
