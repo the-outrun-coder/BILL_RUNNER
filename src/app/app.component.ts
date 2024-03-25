0c3563565dfea6121d9247ff8f3bd5bcd689967f
David Pineda <david.pineda@free4m.io>
2024-03-25 17:22:16 -0400

    Verify Material library is included

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+	imports: [
+		RouterOutlet,
+		MatIconModule,
+		MatMenuModule,
+		MatButtonModule,
+		MatToolbarModule
+	],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
