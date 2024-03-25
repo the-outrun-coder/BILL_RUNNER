9f1dd13cdbca06fe226119f19e285f5e2a1a896c
David Pineda <david.pineda.codes@gmail.com>
2024-03-25 17:57:06 -0400

    Abstract menu and stub PH to LayoutMainComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MatIconModule } from '@angular/material/icon';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatButtonModule } from '@angular/material/button';
-import { MatToolbarModule } from '@angular/material/toolbar';
+
 import { LayoutMainComponent } from './layout/layout-main/layout-main.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
 	imports: [
-		LayoutMainComponent,
 		RouterOutlet,
-		MatIconModule,
-		MatMenuModule,
-		MatButtonModule,
-		MatToolbarModule
+		LayoutMainComponent,
 	],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
--- src/app/layout/layout-main/layout-main.component.ts
+++ src/app/layout/layout-main/layout-main.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'brun-layout-main',
   standalone: true,
-  imports: [],
+	imports: [
+		MatIconModule,
+		MatMenuModule,
+		MatButtonModule,
+		MatToolbarModule
+	],
   templateUrl: './layout-main.component.html',
   styleUrl: './layout-main.component.scss'
 })
