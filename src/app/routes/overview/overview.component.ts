5d5028d3ce2e84a09b4d3e9e30b34f7d46265772
David Pineda <david.pineda.codes@gmail.com>
2024-03-26 16:49:01 -0400

    ng generate component feature/calendar/events + component stub

--- src/app/routes/overview/overview.component.ts
+++ src/app/routes/overview/overview.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { EventsComponent } from '../../feature/calendar/events/events.component';
 
 @Component({
   selector: 'app-overview',
   standalone: true,
-  imports: [RouterOutlet],
+	imports: [
+		EventsComponent,
+		RouterOutlet
+	],
   templateUrl: './overview.component.html',
   styleUrl: './overview.component.scss'
 })
