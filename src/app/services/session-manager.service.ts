811619d27522bbd993008040469bdf295db41911
David Pineda <david.pineda.codes@gmail.com>
2024-04-21 21:24:03 -0400

    dev planning for the session-manager service

--- src/app/services/session-manager.service.ts
+++ src/app/services/session-manager.service.ts
@@ -6,4 +6,14 @@ import { Injectable } from '@angular/core';
 export class SessionManagerService {
 
   constructor() { }
+
+  // TODO - MOCK DATA
+  // TODO - USE NGRX DATA FLOW MNGT
+  // TODO - USE AXIOS DATA FLOW MNGT
+  // TODO - MOCK SERVICE REQUEST
+
+  createSession = () => {}
+  updateSession = () => {}
+  activateSession = () => {}
+  deleteSession = () => {}
 }
